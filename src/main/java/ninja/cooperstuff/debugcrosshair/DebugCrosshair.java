5d1ab442e44fc413f1103108c3d465a70d4254ad
Cooper Anderson <cooper@cooperstuff.ninja>
2018-07-01 18:44:11 -0700

    Stylization

--- src/main/java/ninja/cooperstuff/debugcrosshair/DebugCrosshair.java
+++ src/main/java/ninja/cooperstuff/debugcrosshair/DebugCrosshair.java
@@ -8,8 +8,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = DebugCrosshair.MODID, name = DebugCrosshair.NAME, version = DebugCrosshair.VERSION)
-public class DebugCrosshair
-{
+public class DebugCrosshair {
     public static final String MODID = "debugcrosshair";
     public static final String NAME = "Debug Crosshair";
     public static final String VERSION = "1.0";
@@ -17,14 +16,12 @@ public class DebugCrosshair
     private static Logger logger;
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
+    public void preInit(FMLPreInitializationEvent event) {
         logger = event.getModLog();
     }
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+    public void init(FMLInitializationEvent event) {
 
     }
 }
