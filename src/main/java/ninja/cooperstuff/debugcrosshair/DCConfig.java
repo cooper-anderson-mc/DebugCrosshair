f39fecd173e64d8ae443801c6a9bbd707074b5fc
Cooper Anderson <cooper@cooperstuff.ninja>
2018-07-01 20:02:58 -0700

    Added render options to config

--- src/main/java/ninja/cooperstuff/debugcrosshair/DCConfig.java
+++ src/main/java/ninja/cooperstuff/debugcrosshair/DCConfig.java
@@ -12,6 +12,64 @@ public class DCConfig {
 	@Config.Comment({"Simple way to toggle all features", "(without restarting)"})
 	public static boolean enabled = true;
 
+
+	@Config.Name("Render Options")
+	@Config.Comment("Configure the color and opacity")
+	public static RenderOptions renderOptions;
+
+	public static final class RenderOptions {
+		@Config.Name("X Axis")
+		@Config.Comment("Render options for the X axis")
+		public static Axis xAxis = new Axis(new Color(255, 0, 0, 255), 10);
+		@Config.Name("Y Axis")
+		@Config.Comment("Render options for the Y axis")
+		public static Axis yAxis = new Axis(new Color(0, 255, 0, 255), 10);
+		@Config.Name("Z Axis")
+		@Config.Comment("Render options for the Z axis")
+		public static Axis zAxis = new Axis(new Color(0, 0, 255, 255), 10);
+		@Config.RangeInt(min=0, max=255)
+		public static int opacity = 255;
+		@Config.Comment("Scale the entire model")
+		public static int scale = 1;
+
+		public static final class Axis {
+			@Config.Comment("Color of the axis")
+			public Color color;
+			@Config.Comment("Outline color of the axis")
+			public Color outline;
+			@Config.Comment("Length of the axis")
+			public int length;
+
+			public Axis(Color color, int length) {
+				this.color = color;
+				this.outline = new Color(0, 0, 0, 255);
+				this.length = length;
+			}
+		}
+
+		public static final class Color {
+			@Config.RangeInt(min=0, max=255)
+			@Config.Comment("Red channel of the color")
+			public int red;
+			@Config.RangeInt(min=0, max=255)
+			@Config.Comment("Green channel of the color")
+			public int green;
+			@Config.RangeInt(min=0, max=255)
+			@Config.Comment("Blue channel of the color")
+			public int blue;
+			@Config.RangeInt(min=0, max=255)
+			@Config.Comment("Alpha channel of the color")
+			public int alpha;
+
+			public Color(int red, int green, int blue, int alpha) {
+				this.red = red;
+				this.green = green;
+				this.blue = blue;
+				this.alpha = alpha;
+			}
+		}
+	}
+
 	public static void saveConfig() {
 		ConfigManager.sync(DebugCrosshair.MODID, Config.Type.INSTANCE);
 	}
