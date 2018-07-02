407f3d75d9f03ef071867d5cc4c153a165554a54
Cooper Anderson <cooper@cooperstuff.ninja>
2018-07-02 00:09:31 -0700

    Version Bump because Twitch

--- src/main/java/ninja/cooperstuff/debugcrosshair/DebugCrosshair.java
+++ src/main/java/ninja/cooperstuff/debugcrosshair/DebugCrosshair.java
@@ -20,7 +20,7 @@ import org.lwjgl.opengl.GL11;
 public class DebugCrosshair {
     public static final String MODID = "debugcrosshair";
     public static final String NAME = "Debug Crosshair";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.1";
 
     protected static Logger logger;
     protected static Minecraft mc;
